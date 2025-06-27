33b02a61df2dbb14dcc7a5a27ea389338c43a8fd
mrhardy12 <mrhardy12@gmail.com>
2025-06-27 14:35:35 -0400

    handled Player objects

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    Player.containers = (updatables, drawables)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -12,7 +19,13 @@ def main():
                 return
         
         screen.fill("black")
+        for player in drawables:
+            player.draw(screen)
+        updatables.update(dt)
         pygame.display.flip()
+        
+        # 60 FPS cap
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
